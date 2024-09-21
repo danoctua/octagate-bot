66e2860bac6e0f7594b50bc60f1f8dbddad22b78
danoctua <danik4tl@gmail.com>
2024-09-21 22:03:58 +0200

    chore: Test with longer expiry

--- core/utils/date.py
+++ core/utils/date.py
@@ -2,4 +2,4 @@ from datetime import datetime, timedelta
 
 
 def generate_expire_date(expire_in: int) -> datetime:
-    return datetime.utcnow() + timedelta(minutes=expire_in)
+    return datetime.utcnow() + timedelta(hours=expire_in)
