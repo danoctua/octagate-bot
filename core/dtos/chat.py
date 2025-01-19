030a9c26a951b249f101b8a6db427a9731c0fa68
danoctua <danoctua@gmail.com>
2025-01-19 22:32:45 +0100

    feat: Chat join eligibility service

---
+++ core/dtos/chat.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+from core.models import TelegramChatJetton, TelegramChatNFTCollection
+
+
+class TelegramChatEligibilityRules(BaseModel):
+    jettons: list[TelegramChatJetton]
+    nft_collections: list[TelegramChatNFTCollection]
+
+
+class TelegramChatJettonRule(BaseModel):
+    chat_id: int
+    address: str
+    threshold: int
+    whale_threshold: int
+    whale_custom_label: str
+
+
+class TelegramChatNFTCollectionRule(BaseModel):
+    chat_id: int
+    address: str
--- core/services/chat.py
+++ core/services/chat.py
@@ -1,9 +1,22 @@
 import logging
+from abc import ABC, abstractmethod
 
 from sqlalchemy.exc import NoResultFound
 from telethon.tl.types import Channel
 
-from core.models.chat import TelegramChat, TelegramChatUser
+from core.dtos.chat import (
+    TelegramChatEligibilityRules,
+    TelegramChatJettonRule,
+    TelegramChatNFTCollectionRule,
+)
+from core.models import NftItem
+from core.models.wallet import JettonWallet
+from core.models.chat import (
+    TelegramChat,
+    TelegramChatUser,
+    TelegramChatJetton,
+    TelegramChatNFTCollection,
+)
 from core.services.base import BaseService
 
 
@@ -21,7 +34,7 @@ class TelegramChatService(BaseService):
         )
         self.db_session.add(chat)
         self.db_session.commit()
-        logger.info(f"Telegram Chat {chat.title!r} created.")
+        logger.debug(f"Telegram Chat {chat.title!r} created.")
         return chat
 
     def update(
@@ -32,7 +45,7 @@ class TelegramChatService(BaseService):
         chat.is_forum = entity.forum
         chat.logo_path = logo_path
         self.db_session.commit()
-        logger.info(f"Telegram Chat {chat.title!r} updated.")
+        logger.debug(f"Telegram Chat {chat.title!r} updated.")
         return chat
 
     def create_or_update(
@@ -42,7 +55,7 @@ class TelegramChatService(BaseService):
             chat = self.get(chat_id=chat_id)
             return self.update(entity, chat, logo_path=logo_path)
         except NoResultFound:
-            logger.info(
+            logger.debug(
                 f"No Telegram Chat for ID {entity.id!r} found. Creating new Telegram Chat."
             )
             return self.create(chat_id=chat_id, entity=entity, logo_path=logo_path)
@@ -56,7 +69,7 @@ class TelegramChatService(BaseService):
         chat = self.get(chat_id)
         chat.invite_link = invite_link
         self.db_session.commit()
-        logger.info(f"Telegram Chat {chat.title!r} invite link updated.")
+        logger.debug(f"Telegram Chat {chat.title!r} invite link updated.")
         return chat
 
 
@@ -72,7 +85,7 @@ class TelegramChatUserService(BaseService):
         )
         self.db_session.add(chat_user)
         self.db_session.commit()
-        logger.info(f"Telegram Chat User {chat_user!r} created.")
+        logger.debug(f"Telegram Chat User {chat_user!r} created.")
         return chat_user
 
     def get(self, chat_id: int, user_id: int) -> TelegramChatUser:
@@ -90,7 +103,7 @@ class TelegramChatUserService(BaseService):
         chat_user.is_admin = is_admin
         chat_user.is_whale_admin = is_whale_admin
         self.db_session.commit()
-        logger.info(f"Telegram Chat User {chat_user!r} updated.")
+        logger.debug(f"Telegram Chat User {chat_user!r} updated.")
         return chat_user
 
     def create_or_update(
@@ -102,7 +115,157 @@ class TelegramChatUserService(BaseService):
                 chat_user=chat_user, is_admin=is_admin, is_whale_admin=is_whale_admin
             )
         except NoResultFound:
-            logger.info(
+            logger.debug(
                 f"No Telegram Chat User for chat_id {chat_id!r} and user_id {user_id!r} found. Creating new Telegram Chat User."
             )
             return self.create(chat_id, user_id, is_admin, is_whale_admin)
+
+    def get_eligibility_rules(self, chat_id: int) -> TelegramChatEligibilityRules:
+        """
+        Get eligibility rules for the chat based on the database records
+        :param chat_id:
+        :return:
+        """
+        telegram_chat_jetton_service = TelegramChatJettonService(self.db_session)
+        all_jetton_rules = telegram_chat_jetton_service.get_all(chat_id)
+        telegram_chat_nft_collection_service = TelegramChatNFTCollectionService(
+            self.db_session
+        )
+        all_nft_collections = telegram_chat_nft_collection_service.get_all(chat_id)
+        return TelegramChatEligibilityRules(
+            jettons=all_jetton_rules,
+            nft_collections=all_nft_collections,
+        )
+
+    @classmethod
+    def is_user_eligible_chat_member(
+        cls,
+        eligibility_rules: TelegramChatEligibilityRules,
+        user_jettons: list[JettonWallet],
+        user_nft_items: list[NftItem],
+    ) -> bool:
+        """
+        The rule is to have all required jetton balance OR all NFT items from the required collections
+
+        :param eligibility_rules: Rules for eligibility to join the chat
+        :param user_jettons: Jetton balances of the user
+        :param user_nft_items: NFT items of the user
+        :return: True if the user is eligible to join the chat, False otherwise
+        """
+        has_all_jettons_required = True
+        user_jettons_by_master_address = {
+            jetton_wallet.jetton_master_address: jetton_wallet
+            for jetton_wallet in user_jettons
+        }
+        for rule in eligibility_rules.jettons:
+            if not (
+                user_jetton_wallet := user_jettons_by_master_address.get(
+                    rule.jetton_address
+                )
+            ):
+                has_all_jettons_required = False
+                break
+
+            if not user_jetton_wallet.balance >= rule.threshold:
+                has_all_jettons_required = False
+                break
+
+        if has_all_jettons_required:
+            return True
+
+        user_nft_items_by_collection_address = {
+            nft_item.collection_address: nft_item for nft_item in user_nft_items
+        }
+        return all(
+            nft_item.collection_address in user_nft_items_by_collection_address
+            for nft_item in eligibility_rules.nft_collections
+        )
+
+
+TelegramChatRuleType = TelegramChatJetton | TelegramChatNFTCollection
+TelegramChatRuleDTOType = TelegramChatJettonRule | TelegramChatNFTCollectionRule
+
+
+class TelegramChatRuleBaseService(BaseService, ABC):
+    model: type[TelegramChatRuleType]
+    dto: type[TelegramChatJettonRule]
+
+    def create(self, dto: TelegramChatRuleDTOType) -> TelegramChatRuleType:
+        new_rule = self.model(**dto.model_dump())
+        self.db_session.add(new_rule)
+        self.db_session.commit()
+        logger.debug(f"Telegram Chat Rule {new_rule!r} created.")
+        return new_rule
+
+    @abstractmethod
+    def get(self, chat_id: int, address: str) -> TelegramChatRuleType:
+        ...
+
+    def get_all(
+        self, chat_id: int | None = None, enabled_only: bool = False
+    ) -> list[TelegramChatRuleType]:
+        query = self.db_session.query(self.model)
+        if chat_id is not None:
+            query = query.filter(self.model.chat_id == chat_id)
+
+        if enabled_only:
+            query = query.filter(self.model.is_enabled.is_(True))
+
+        return query.all()
+
+    def enable_rule(self, chat_id: int, address: str) -> None:
+        rule = self.get(chat_id, address)
+        rule.is_enabled = True
+        self.db_session.commit()
+        logger.debug(f"Telegram Chat Rule {rule!r} enabled.")
+
+    def disable_rule(self, chat_id: int, address: str) -> None:
+        rule = self.get(chat_id, address)
+        rule.is_enabled = False
+        self.db_session.commit()
+        logger.debug(f"Telegram Chat Rule {rule!r} disabled.")
+
+
+class TelegramChatJettonService(TelegramChatRuleBaseService):
+    model = TelegramChatJetton
+    dto = TelegramChatJettonRule
+
+    def update(
+        self,
+        chat_id: int,
+        address: str,
+        threshold: int,
+        whale_threshold: int | None = None,
+        whale_label_template: str | None = None,
+    ) -> None:
+        telegram_chat_jetton = self.get(chat_id, address)
+        telegram_chat_jetton.threshold = threshold
+        telegram_chat_jetton.whale_threshold = whale_threshold
+        telegram_chat_jetton.whale_label_template = whale_label_template
+        self.db_session.commit()
+        logger.debug(f"Telegram Chat Jetton {telegram_chat_jetton!r} updated.")
+
+    def get(self, chat_id: int, jetton_address: str) -> type[TelegramChatJetton]:
+        return (
+            self.db_session.query(TelegramChatJetton)
+            .filter(
+                TelegramChatJetton.chat_id == chat_id,
+                TelegramChatJetton.jetton_address == jetton_address,
+            )
+            .one()
+        )
+
+
+class TelegramChatNFTCollectionService(TelegramChatRuleBaseService):
+    model = TelegramChatNFTCollection
+    dto = TelegramChatNFTCollectionRule
+
+    def get(self, chat_id: int, collection_address: str) -> TelegramChatNFTCollection:
+        return (
+            self.db_session.query(TelegramChatNFTCollection)
+            .filter(
+                TelegramChatNFTCollection.chat_id == chat_id,
+                TelegramChatNFTCollection.collection_address == collection_address,
+            )
+            .one()
+        )
