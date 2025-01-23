5da2a847efa38cdf280b1163d1e36747c81a2061
danoctua <danik4tl@gmail.com>
2025-01-23 20:03:06 +0100

    chore: default first name to empty string

--- core/dtos/user.py
+++ core/dtos/user.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 class TelegramUserDTO(BaseModel):
     id: int
-    first_name: str
+    first_name: str = ""
     last_name: str | None = None
     username: str | None = None
     is_premium: bool
