e62696089473dcdcfe80e49a74257cdff44cf5ce
danoctua <danik4tl@gmail.com>
2025-02-17 20:04:04 +0100

    fix: Route webhook traffic to the /webhook path

--- bot_ui/handlers/command/__init__.py
+++
@@ -1,9 +0,0 @@
-from telegram.ext import CommandHandler, filters
-
-from bot_ui.handlers.command.start import start_handler
-
-__all__ = ["handlers"]
-
-handlers = [
-    CommandHandler("start", start_handler, filters=filters.ChatType.PRIVATE),
-]
--- bot_ui/settings.py
+++ bot_ui/settings.py
@@ -6,6 +6,7 @@ DEFAULT_API_BASE_URL = "https://api.telegram.org/bot"
 class BotUiSettings(CoreSettings):
     telegram_api_base_url: str = DEFAULT_API_BASE_URL
 
+    webhook_prefix: str = "/webhook"
     webhook_url: str
     webhook_secret_key: str
     webhook_host: str = "0.0.0.0"
@@ -23,7 +24,7 @@ class BotUiSettings(CoreSettings):
 
     @property
     def full_webhook_url(self):
-        return f"{bot_ui_settings.webhook_url}/{bot_ui_settings.telegram_bot_token}"
+        return f"{bot_ui_settings.webhook_prefix}/{bot_ui_settings.webhook_url}/{bot_ui_settings.telegram_bot_token}"
 
 
 bot_ui_settings = BotUiSettings()
