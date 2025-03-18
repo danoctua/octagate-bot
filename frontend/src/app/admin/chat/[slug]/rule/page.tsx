bfbf6f8e99154018fd406e91be389996cffc2905
danoctua <danik4tl@gmail.com>
2025-03-18 13:41:58 +0100

    chore: Minor UI fixes

--- frontend/src/app/admin/chat/[slug]/rule/page.tsx
+++ frontend/src/app/admin/chat/[slug]/rule/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import {Page} from "@/components/layout/Page";
-import {SegmentedControl, Select, Title} from "@telegram-apps/telegram-ui";
+import {SegmentedControl, Title} from "@telegram-apps/telegram-ui";
 import {useMemo, useState} from "react";
 import JettonRule from "@/components/layout/Rule/JettonRule/JettonRule";
 import NftCollectionRule from "@/components/layout/Rule/NftCollectionRule/NftCollectionRule";
 import WhitelistRule from "@/components/layout/Rule/WhitelistRule/WhitelistRule";
 import WhitelistExternalRule from "@/components/layout/Rule/WhitelistExternalRule/WhitelistExternalRule";
+import {miniApp, useSignal} from "@telegram-apps/sdk-react";
 
 
 const TABS = [
@@ -19,6 +20,7 @@ const TABS = [
 
 const NewRulePage = ({params}: { params: { slug: string } }) => {
     const [selectedTab, setSelectedTab] = useState(TABS[0].type);
+    const isDark = useSignal(miniApp.isDark);
 
     const renderTab = useMemo(
         () => {
@@ -40,25 +42,30 @@ const NewRulePage = ({params}: { params: { slug: string } }) => {
     return (
         <Page back={true}>
             <>
-                <div style={{padding: "44px 32px"}}>
-                    <Title level={"1"} weight={"1"} style={{textAlign: "center"}}>
-                        Add condition
-                    </Title>
-                </div>
+                <div>
+                    <div style={{padding: "44px 32px"}}>
+                        <Title level={"1"} weight={"1"} style={{textAlign: "center"}}>
+                            Add condition
+                        </Title>
+                    </div>
 
-                <SegmentedControl>
-                    {
-                        TABS.map((tab, index) => (
-                            <SegmentedControl.Item
-                                key={index}
-                                selected={selectedTab === tab.type}
-                                onClick={() => setSelectedTab(tab.type)}
-                            >
-                                {tab.title}
-                            </SegmentedControl.Item>
-                        ))
-                    }
-                </SegmentedControl>
+                    <SegmentedControl
+                        style={{background: isDark ? "#2F2F2F" : "#F6F6FA"}}
+                    >
+                        {
+                            TABS.map((tab, index) => (
+                                <SegmentedControl.Item
+                                    style={{background: selectedTab === tab.type ? "var(--tgui--bg_color)" : "transparent"}}
+                                    key={index}
+                                    selected={selectedTab === tab.type}
+                                    onClick={() => setSelectedTab(tab.type)}
+                                >
+                                    {tab.title}
+                                </SegmentedControl.Item>
+                            ))
+                        }
+                    </SegmentedControl>
+                </div>
             </>
 
             {renderTab}
