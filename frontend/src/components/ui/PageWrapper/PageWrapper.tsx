ce8d98899e9b5bc6a17069381ca9295cbf582e72
danoctua <danik4tl@gmail.com>
2025-03-28 13:41:37 +0100

    chore: Updated animate presence animation

--- frontend/src/components/ui/PageWrapper/PageWrapper.tsx
+++ frontend/src/components/ui/PageWrapper/PageWrapper.tsx
@@ -1,23 +1,26 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
-import { usePathname } from "next/navigation";
+import {motion, AnimatePresence} from "framer-motion";
+import {usePathname} from "next/navigation";
 import {ReactNode} from "react";
 
-export default function PageWrapper({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
+export default function PageWrapper({children}: { children: ReactNode }) {
+    const pathname = usePathname();
 
-  return (
-    <AnimatePresence mode="sync">
-      <motion.div
-        key={pathname} // Animates on page change
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -10 }}
-        transition={{ duration: 0.3 }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  );
+    return (
+        // For the home page, we don't want to animate
+        pathname === '/' ?
+            <>{children}</> :
+            <AnimatePresence mode="popLayout">
+                <motion.div
+                    key={pathname} // Animates on page change
+                    initial={{opacity: 0, x: 10}}
+                    animate={{opacity: 1, x: 0}}
+                    exit={{opacity: 0, x: -10}}
+                    transition={{duration: 0.3}}
+                >
+                    {children}
+                </motion.div>
+            </AnimatePresence>
+    );
 }
