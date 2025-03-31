af5526e6a005418e4233b51ad73b2ea09f183c43
danoctua <danik4tl@gmail.com>
2025-03-31 13:48:08 +0200

    chore: Rewrite public/dynamic path to prevent attempts to render as a page

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,16 @@
 import {withSentryConfig} from '@sentry/nextjs';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/dynamic/:path*",
+                destination: "/public/dynamic/:path*",
+            }
+        ]
+    }
+}
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
